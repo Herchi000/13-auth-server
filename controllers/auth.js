fff4079d171929463cb5e72b27265e2dd35b363d
unknown <hugofer000@gmail.com>
2022-07-22 18:03:05 -0300

    13-auth-server 01

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,9 @@
 const { response} = require('express');
-const { validationResult } = require('express-validator');
+
 
 const crearUsuario = (req, res = response) => {
 
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        })
-    }
-
-    console.log(req.body);
+    //console.log(req.body);
 
     return res.json({
         ok: true,
@@ -22,14 +14,6 @@ const crearUsuario = (req, res = response) => {
 
 
 const loginUsuario = (req, res = response) => { //el response es unicamente para darle tipado al res
-
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        });
-    }
 
 
     const { email, password } = req.body;
